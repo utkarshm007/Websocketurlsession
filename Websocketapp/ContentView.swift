4acdf6308ac9ce0178d8d6e0bb5fc067fe3e167a
Mishra, Utkarsh <utkarsh.mishra01@sap.com>
2025-04-15 11:22:09 +0530

    Initial Commit

---
+++ Websocketapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Websocketapp
+//
+//  Created by Mishra, Utkarsh on 15/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Websocketapp/WebsocketappApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebsocketappApp.swift
+//  Websocketapp
+//
+//  Created by Mishra, Utkarsh on 15/04/25.
+//
+
+import SwiftUI
+
+@main
+struct WebsocketappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
