9d39274474d987743dc507ba258c860409bcdd14
Mishra, Utkarsh <utkarsh.mishra01@sap.com>
2025-04-16 14:47:52 +0530

    Initial Commit

---
+++ Websocketurlsession/WebsocketurlsessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebsocketurlsessionApp.swift
+//  Websocketurlsession
+//
+//  Created by Mishra, Utkarsh on 16/04/25.
+//
+
+import SwiftUI
+
+@main
+struct WebsocketurlsessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WebsocketurlsessionTests/WebsocketurlsessionTests.swift
@@ -0,0 +1,17 @@
+//
+//  WebsocketurlsessionTests.swift
+//  WebsocketurlsessionTests
+//
+//  Created by Mishra, Utkarsh on 16/04/25.
+//
+
+import Testing
+@testable import Websocketurlsession
+
+struct WebsocketurlsessionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
